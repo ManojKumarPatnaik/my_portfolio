eaa50474d55ba1dca23eefa9c141214b6ef105b5
Manojkumar_Uriti <u.manoj3p0@gmail.com>
2023-06-14 01:33:51 +0530

    support

--- src/index.js
+++ src/index.js
@@ -5,15 +5,3 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
-<script>
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-    var mailtoLink = 'mailto:u.manoj3p0@gmail.com' +
-      '?subject=' + encodeURIComponent('New Contact Form Submission') +
-      '&body=' + encodeURIComponent('Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
-    window.location.href = mailtoLink;
-  });
-</script>
